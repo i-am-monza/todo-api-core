9d09fafae50b365b40844b02827edbe3bf96ac3d
Monwabisi Dingane <monwabisid@globalkinetic.com>
2020-06-05 13:41:15 +0200

    This point is just before refactoring

---
+++ TodoApi/Controllers/ITodoItemsController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+using TodoApi.Exceptions;
+
+namespace TodoApi.Controllers
+{
+    public interface ITodoItemsController
+    {
+        public Task<ActionResult<IEnumerable<TodoItemDTO>>> GetTodoItems();
+
+        public Task<ActionResult<TodoItemDTO>> GetTodoItem(long id);
+
+        public Task<ActionResult<TodoItemDTO>> UpdateTodoItem(long id, TodoItemDTO todoItemDTO);
+
+        public Task<ActionResult<TodoItemDTO>> CreateTodoItem(TodoItemDTO todoItemDTO);
+
+        public Task<ActionResult<TodoItemDTO>> DeleteTodoItem(long id);
+    }
+}
