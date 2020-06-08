f9a9d4f37d7377ccec1d13db3a161ff4056c014f
Monwabisi Dingane <monwabisid@globalkinetic.com>
2020-06-09 01:23:30 +0200

    Update solution name

--- TodoItemsControllerTest/TodoItemsControllerTest.cs
+++ TodoItemsControllerTest/TodoItemsControllerTest.cs
@@ -5,17 +5,23 @@ using TodoApi.Controllers;
 using TodoApi.Models;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace TodoItemsUnitTest
 {
     public class TodoItemsControllerTest
     {
         [Fact]
-        public void TestGetRoute_WhenTableEmpty()
+        public void TestGetTodoItems_WhenUserEmpty_ReturnEmptyList()
         {
             // assemble
-
-            var mockGet = new Mock<ITodoItemsController>();
+            var mockTodoContext = new Mock<TodoContext>(new DbContextOptions<TodoContext>());
+            var todoItemsController = new TodoItemsController(mockTodoContext.Object);
+            // act
+            var result = todoItemsController.GetTodoItems();
+            // assert
+            Assert.IsType<ActionResult<IEnumerable<TodoItemDTO>>>(result);
         }
     }
 }
